ef1ff56a32f30908de75330b1b51e5cb8b735814
Name <user@example.com>
2024-08-22 12:27:38 +0000

    commit msg Closes: #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -2,23 +2,55 @@
 
 import re
 
+
+
 def validate_user(username, minlen):
-    """Checks if the received username matches the required conditions."""
+
+    """Checks if the received username matches the required conditions"""
+
     if type(username) != str:
+
         raise TypeError("username must be a string")
+
     if minlen < 1:
+
         raise ValueError("minlen must be at least 1")
+
     
+
     # Usernames can't be shorter than minlen
+
     if len(username) < minlen:
+
         return False
-    # Usernames can only use letters, numbers, dots and underscores
-    if not re.match('^[a-z0-9._]*$', username):
-        return False
-    # Usernames can't begin with a number
-    if username[0].isnumeric():
+
+    
+
+    # Usernames must start with a letter, and can only use letters, numbers, dots, and underscores
+
+    if not re.match('^[a-zA-Z][a-zA-Z0-9._]*$', username):
+
         return False
     return True
 
 
+print(validate_user("blue.kale", 3)) # True
+
+
+
+print(validate_user(".blue.kale", 3)) # Currently True, should be F$
+
+
+
+print(validate_user("red_quinoa", 4)) # True
+
+
+
+print(validate_user("_red_quinoa", 4)) # Currently True, should be $
+
+
+
+							   
+
+					
 
